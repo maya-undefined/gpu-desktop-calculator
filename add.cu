cbece763975a9dcbe05d2643a9bcf2e133bd35c8
user <user@dash>
2023-11-18 02:38:38 -0800

    stream from files

--- add.cu
+++ add.cu
@@ -3,33 +3,30 @@
 #include <vector>
 #include <cuda_runtime.h>
 
-// Utility function to read data from a file into a vector
-std::vector<float> readDataFromFile(const std::string &filename) {
-    std::vector<float> data;
-    std::ifstream file(filename);
-    float value;
-    while (file >> value) {
-        data.push_back(value);
-    }
-    return data;
-}
+#define _CHUNK_SIZE 1024
 
 class FH {
 private:
     std::ifstream _file;
+    const uint _chunksize = _CHUNK_SIZE;
 public:
     FH(const std::string &filename) {
         _file = std::ifstream(filename);
     }
 
+    bool eof() {
+        return _file.eof();
+    }
+
     std::vector<float> readDataFromFile() {
         std::vector<float> data;
         float value;
-        while (_file >> value) {
+        while (data.size() < _chunksize) {
+            if (!(_file >>value)) break;
             data.push_back(value);
         }
         return data;
-        }
+    }
 };
 
 __global__ void addArrays(const float *A, const float *B, float *C, int numElements) {
@@ -51,33 +48,38 @@ int main(int argc, char *argv[]) {
     FH host_A_file(argv[1]);
     FH host_B_file(argv[2]);
 
-    std::vector<float> host_A = host_A_file.readDataFromFile();
-    std::vector<float> host_B = host_B_file.readDataFromFile();
-    int numElements = host_A.size();
+    // Write result to file
+    std::ofstream outputFile(argv[3]);
+
 
     // Allocate memory on the GPU
     float *device_A, *device_B, *device_C;
-    cudaMalloc((void **)&device_A, numElements * sizeof(float));
-    cudaMalloc((void **)&device_B, numElements * sizeof(float));
-    cudaMalloc((void **)&device_C, numElements * sizeof(float));
+    cudaMalloc((void **)&device_A, _CHUNK_SIZE * sizeof(float));
+    cudaMalloc((void **)&device_B, _CHUNK_SIZE * sizeof(float));
+    cudaMalloc((void **)&device_C, _CHUNK_SIZE * sizeof(float));
 
-    // Copy data from host to device
-    cudaMemcpy(device_A, host_A.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_B, host_B.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
+    while (!host_A_file.eof()) {
+        std::vector<float> host_A = host_A_file.readDataFromFile();
+        std::vector<float> host_B = host_B_file.readDataFromFile();
+        int numElements = host_A.size();
 
-    // Launch the CUDA Kernel
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
-    addArrays<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_B, device_C, numElements);
+        // Copy data from host to device
+        cudaMemcpy(device_A, host_A.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(device_B, host_B.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
 
-    // Copy result back to host
-    std::vector<float> host_C(numElements);
-    cudaMemcpy(host_C.data(), device_C, numElements * sizeof(float), cudaMemcpyDeviceToHost);
+        // Launch the CUDA Kernel
+        int threadsPerBlock = 256;
+        int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+        addArrays<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_B, device_C, numElements);
 
-    // Write result to file
-    std::ofstream outputFile(argv[3]);
-    for (float value : host_C) {
-        outputFile << value << std::endl;
+        // Copy result back to host
+        std::vector<float> host_C(numElements);
+        cudaMemcpy(host_C.data(), device_C, numElements * sizeof(float), cudaMemcpyDeviceToHost);
+
+        for (float value : host_C) {
+            outputFile << value << std::endl;
+        }
+
     }
 
     // Free device memory
