fef7f2299ef25956060b63fca10d8a1947c73ccd
user <user@dash>
2023-11-23 22:43:37 -0800

    towards rows and columns for kernel

--- add.cu
+++ add.cu
@@ -22,10 +22,12 @@ public:
     std::vector<float> parse_line_of_floats(const std::string& line) {
         std::vector<float> numbers;
         const char* str = line.c_str();
-        char* end;
+        char* end = NULL;
 
         while (true) {
             float num = std::strtof(str, &end);
+            // is this safe?
+
             if ( end == str) break;
             numbers.push_back(num);
             str = end;
@@ -41,7 +43,7 @@ public:
         return total_size;
     }
 
-    std::vector<float> readDataFromFile() {
+    std::vector<std::vector<float> > readDataFromFile() {
         std::vector<std::vector<float> > data;
         std::string line;
 
@@ -63,9 +65,27 @@ public:
             total_size = total_vector_size(data);
         }
 
-        return data[0];
+        return data;
     }
 };
+
+__global__ void addMultipleArrays(float **A, float **B, float *C, int A_s, int B_s, int numElements) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < numElements) {
+        float sum_A = 0;
+        float sum_B = 0;
+        for (int j = 0; j < A_s; ++j) {
+            sum_A += A[i][j];
+        }
+
+        for (int j = 0; j < A_s; ++j) {
+            sum_B += B[i][j];
+        }
+
+        
+        C[i] = sum_A + sum_B;
+    }
+}
 
 __global__ void addArrays(const float *A, const float *B, float *C, int numElements) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
@@ -82,7 +102,7 @@ __global__ void complexPhysicsCalculation(const float *A, const float *B, float*
         float result = sinf(A[i]) * cosf(B[i]) + temp;
 
         // Store the result
-        C [i]= result;
+        C[i]= result;
     }
 }
 
@@ -104,26 +124,64 @@ int main(int argc, char *argv[]) {
     char* buffer = new char[_CHUNK_SIZE];
     outputFile.rdbuf()->pubsetbuf(buffer, _CHUNK_SIZE);
 
-    // Allocate memory on the GPU
-    float *device_A, *device_B, *device_C;
-    cudaMalloc((void **)&device_A, _CHUNK_SIZE * sizeof(float));
-    cudaMalloc((void **)&device_B, _CHUNK_SIZE * sizeof(float));
-    cudaMalloc((void **)&device_C, _CHUNK_SIZE * sizeof(float));
+
+    // cudaMalloc((void **)&device_A, _CHUNK_SIZE * sizeof(float));
+    // cudaMalloc((void **)&device_B, _CHUNK_SIZE * sizeof(float));
 
     while (!host_A_file.eof()) {
-        std::vector<float> host_A = host_A_file.readDataFromFile();
-        std::vector<float> host_B = host_B_file.readDataFromFile();
+        std::vector<std::vector<float> > host_A = host_A_file.readDataFromFile();
+        std::vector<std::vector<float> > host_B = host_B_file.readDataFromFile();
         int numElements = host_A.size();
 
-        // Copy data from host to device
-        cudaMemcpy(device_A, host_A.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(device_B, host_B.data(), numElements * sizeof(float), cudaMemcpyHostToDevice);
+        // Allocate memory on the GPU
+        // these will memoryleak
+        float *device_C;
+        // float *device_A[numElements], *device_B[numElements];
+        float **device_A, **device_B;
+
+        cudaMalloc((void **)&device_C, _CHUNK_SIZE * sizeof(float));
+
+        std::vector<float> flat_A;
+        std::vector<float> flat_B;
+        for (const auto& row: host_A) {
+            flat_A.insert(flat_A.end(), row.begin(), row.end());
+        }
+
+        for (const auto& row: host_B) {
+            flat_B.insert(flat_B.end(), row.begin(), row.end());
+        }
+        
+        cudaMalloc(&device_A, flat_A.size() * sizeof(float));
+        cudaMalloc(&device_B, flat_B.size() * sizeof(float));
+        cudaMemcpy(device_A, flat_A.data(), flat_A.size() * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(device_B, flat_B.data(), flat_B.size() * sizeof(float), cudaMemcpyHostToDevice);
+
+        // // Copy data from host to device
+        // for (int i = 0 ; i < host_A.size(); i++) {
+        //     // for (int j = 0; j < host_A[i].size(); j++)
+        //     //     std::cout << host_A[i][j];
+        //     // std::cout << "\n";
+        //     device_A[i] = new float(host_A[i].size());
+        //     cudaMalloc(&device_A[i], host_A[i].size() * sizeof(float));
+        //     cudaMemcpy(device_A[i], host_A[i].data(), host_A[i].size() * sizeof(float), cudaMemcpyHostToDevice);
+        // }
+
+        // for (int i = 0 ; i < host_B.size(); i++) {
+        //     // for (int j = 0; j < host_B[i].size(); j++)
+        //     //     std::cout << host_B[i][j];
+        //     // std::cout << "\n";
+        //     device_B[i] = new float(host_B[i].size());
+        //     cudaMalloc(&device_B[i], host_B[i].size() * sizeof(float));
+        //     cudaMemcpy(device_B[i], host_B[i].data(), host_B[i].size() * sizeof(float), cudaMemcpyHostToDevice);
+        // }
 
         // Launch the CUDA Kernel
-        int threadsPerBlock = 256;
-        int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
-        addArrays<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_B, device_C, numElements);
-        // complexPhysicsCalculation<<<blocksPerGrid, threadsPerBlock>>>(device_A, device_B, device_C, numElements);
+        dim3 blockSize(256);
+        dim3 gridSize((numElements + blockSize.x - 1) / blockSize.x);
+        addMultipleArrays<<<gridSize, blockSize>>>(
+                device_A, device_B, device_C, 
+                host_A.size(), host_B.size(), // rows
+                host_A[0].size());            // columns
 
         // Copy result back to host
         std::vector<float> host_C(numElements);
@@ -133,12 +191,12 @@ int main(int argc, char *argv[]) {
             outputFile << value << "\n";
         }
 
+        cudaFree(device_A);
+        cudaFree(device_B);
+        cudaFree(device_C);
     }
 
-    // Free device memory
-    cudaFree(device_A);
-    cudaFree(device_B);
-    cudaFree(device_C);
+
 
     return 0;
 }
