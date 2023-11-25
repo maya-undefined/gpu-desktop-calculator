48b6eca6d809121cf016f27f13778353c685c32b
user <user@dash>
2023-11-25 13:39:37 -0800

    2 cols, 1 col works for small inputs

--- add.cu
+++ add.cu
@@ -3,7 +3,8 @@
 #include <vector>
 #include <cuda_runtime.h>
 
-#define _CHUNK_SIZE 1024 * 1024 * 50
+// #define _CHUNK_SIZE 1024 * 1024 * 50
+#define _CHUNK_SIZE 1
 
 class FH {
 private:
@@ -78,7 +79,7 @@ public:
 
             data.insert(data.end(), numbers.begin(), numbers.end());
             _row_len += 1;
-            total_size += numbers.size();
+            total_size += numbers.size() * sizeof(float);
         }
 
         return data;
@@ -145,31 +146,41 @@ int main(int argc, char *argv[]) {
     float *device_C;
     float *device_A, *device_B;
 
+    size_t A_rows, A_cols, B_rows, B_cols;
+    A_rows = 0; A_cols = 1; B_rows = 0; B_cols = 1;
     while (!host_A_file.eof()) {
         std::vector<float> host_A = host_A_file.read_data_from_file();
         std::vector<float> host_B = host_B_file.read_data_from_file();
         int numElements = host_A.size();
 
-        cudaMalloc((void **)&device_C, _CHUNK_SIZE * sizeof(float));
+        A_rows = host_A_file.row_len() - A_rows;
+        B_rows = host_B_file.row_len() - B_rows;
+
+        if (B_cols != host_B_file.col_len()) { B_cols = host_B_file.col_len(); }
+        if (A_cols != host_A_file.col_len()) { A_cols = host_A_file.col_len(); }
+
+        cudaMalloc((void **)&device_C, A_rows*A_cols * sizeof(float));
 
         
-        cudaMalloc(&device_A, host_A.size() * sizeof(float));
-        cudaMalloc(&device_B, host_B.size() * sizeof(float));
-        cudaMemcpy(device_A, host_A.data(), host_A.size() * sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(device_B, host_B.data(), host_B.size() * sizeof(float), cudaMemcpyHostToDevice);
+        // cudaMalloc(&device_A, host_A.size() * sizeof(float));
+        // cudaMalloc(&device_B, host_B.size() * sizeof(float));
+        cudaMalloc((void **)&device_A, A_rows*A_cols * sizeof(float));
+        cudaMalloc((void **)&device_B, host_B_file.row_len()*host_B_file.col_len() * sizeof(float));
+        cudaMemcpy(device_A, host_A.data(), A_rows*A_cols * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(device_B, host_B.data(), B_rows*B_cols * sizeof(float), cudaMemcpyHostToDevice);
 
         // Launch the CUDA Kernel
         dim3 blockSize(256);
-        dim3 gridSize((numElements + blockSize.x - 1) / blockSize.x);
+        dim3 gridSize((A_rows + blockSize.x - 1) / blockSize.x);
         addMultipleArrays<<<gridSize, blockSize>>>(
                 device_A, device_B, device_C, 
-                host_A_file.row_len(), host_B_file.row_len(), // rows
-                host_A_file.col_len(), host_B_file.col_len() // columns
+                A_rows, B_rows, // rows
+                A_cols, B_cols // columns
                 ); 
 
         // Copy result back to host
-        std::vector<float> host_C(host_A_file.row_len());
-        cudaMemcpy(host_C.data(), device_C, host_A_file.row_len() * sizeof(float), cudaMemcpyDeviceToHost);
+        std::vector<float> host_C(min(host_A.size(), host_B.size()));
+        cudaMemcpy(host_C.data(), device_C, A_rows * A_cols * sizeof(float), cudaMemcpyDeviceToHost);
 
         for (float value : host_C) {
             outputFile << value << "\n";
